c6fbf13650a7f765bc0d33f09db81428ec3790ee
Paul <kappmeyer.p@gmail.com>
2020-02-17 21:01:47 +0100

    Smoother camera movement

--- Eden-Jump/src/gameengine/graphics/Camera.java
+++ Eden-Jump/src/gameengine/graphics/Camera.java
@@ -7,21 +7,37 @@ import gamelogic.player.Player;
 public class Camera {
 
 	private Vector2D position;
-	private Player focusedObject;
+	private Player player;
+	
+	private float velocity = 2.0f; 
+	private float setValue = 0.5f;
 	
 	public Camera() {
 		this.position = new Vector2D();
 	}
 	
 	public void update(float tslf) {
-		if(focusedObject != null) {
-			position.x = focusedObject.getX() + focusedObject.getWidth()/2 - Main.SCREEN_WIDTH/2;
-			position.y = focusedObject.getY() + focusedObject.getHeight()/2 - Main.SCREEN_HEIGHT/2;
+		if(player != null) {
+			float goalX = player.getX() + player.getWidth()/2 - Main.SCREEN_WIDTH/2;
+			float goalY = player.getY() + player.getHeight()/2 - Main.SCREEN_HEIGHT/2;
+			
+			//Calculating the differences between the goal position and the current position of the camera
+			float diffX = goalX - position.x;
+			float amountX = diffX * velocity;
+			position.x += amountX * tslf;
+
+			float diffY = goalY - position.y;
+			float amountY = diffY * velocity;
+			position.y += amountY * tslf;
+			
+			//At this point the difference is too small so the value gets set to avoid shaking of the camera
+			if(-setValue < diffX && diffX < setValue) position.x = goalX;
+			if(-setValue < diffY && diffY < setValue) position.y = goalY;
 		}
 	}
 	
 	public void setFocusedObject(Player object) {
-		this.focusedObject = object;
+		this.player = object;
 	}
 	
 	public boolean isVisibleOnCamera(float x, float y, int width, int height) {
